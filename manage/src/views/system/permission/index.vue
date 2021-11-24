58de27da58f148c51eacf3e8d44a39983cff5b77
cgh <1036985893@qq.com>
2021-11-24 17:29:57 +0800

    新增权限管理页面

---
+++ manage/src/views/system/permission/index.vue
@@ -0,0 +1,139 @@
+<template>
+  <div class="app-container">
+    <div class="block">
+      <el-row :gutter="20">
+        <el-col :span="6">
+          <el-input v-model="listQuery.name" placeholder="请输入权限名称"></el-input>
+        </el-col>
+        <el-col :span="6">
+          <el-button type="success" icon="el-icon-search" @click.native="search">{{ $t('button.search') }}</el-button>
+          <el-button type="primary" icon="el-icon-refresh" @click.native="reset">{{ $t('button.reset') }}</el-button>
+        </el-col>
+      </el-row>
+      <br>
+      <el-row>
+        <el-col :span="24">
+          <el-button type="success" icon="el-icon-plus" @click.native="add">{{ $t('button.add') }}</el-button>
+          <el-button type="primary" icon="el-icon-edit" @click.native="edit">{{ $t('button.edit') }}</el-button>
+          <el-button type="danger" icon="el-icon-delete" @click.native="remove">{{ $t('button.delete') }}</el-button>
+        </el-col>
+      </el-row>
+    </div>
+
+
+    <tree-table
+      :data="list"
+      :expandAll=true
+      v-loading="listLoading"
+      element-loading-text="Loading"
+      border fit highlight-current-row
+      @current-change="handleCurrentChange">
+
+      <el-table-column label="名称">
+        <template slot-scope="scope">
+          {{scope.row.name}}
+        </template>
+      </el-table-column>
+      <el-table-column label="编码">
+        <template slot-scope="scope">
+          {{scope.row.code}}
+        </template>
+      </el-table-column>
+      <el-table-column label="上级权限">
+        <template slot-scope="scope">
+          {{scope.row.pName}}
+        </template>
+      </el-table-column>
+      <el-table-column label="url">
+        <template slot-scope="scope">
+          {{scope.row.url}}
+        </template>
+      </el-table-column>
+      <el-table-column label="所属菜单">
+        <template slot-scope="scope">
+          {{scope.row.menuName}}
+        </template>
+      </el-table-column>
+      <el-table-column label="备注">
+        <template slot-scope="scope">
+          {{scope.row.remark}}
+        </template>
+      </el-table-column>
+
+    </tree-table>
+
+
+    <el-pagination
+      background
+      layout="total, sizes, prev, pager, next, jumper"
+      :page-sizes="[10, 20, 50, 100,500]"
+      :page-size="listQuery.limit"
+      :total="total"
+      @size-change="changeSize"
+      @current-change="fetchPage"
+      @prev-click="fetchPrev"
+      @next-click="fetchNext">
+    </el-pagination>
+
+    <el-dialog
+      :title="formTitle"
+      :visible.sync="formVisible"
+      width="70%">
+      <el-form ref="form" :model="form" :rules="rules" label-width="80px">
+        <el-row>
+          <el-col :span="12">
+            <el-form-item label="名称" prop="name">
+              <el-input v-model="form.name" minlength=1></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="12">
+            <el-form-item label="编码" prop="code">
+              <el-input v-model="form.code" minlength=1></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="12">
+            <el-form-item label="url" prop="code">
+              <el-input v-model="form.url" minlength=5></el-input>
+            </el-form-item>
+          </el-col>
+
+          <el-col :span="12">
+            <el-form-item label="上级权限">
+              <el-input
+                placeholder="请选择上级权限"
+                v-model="form.pName"
+                readonly="readonly"
+                @click.native="permissionTree.show = !permissionTree.show">
+              </el-input>
+              <el-tree v-if="permissionTree.show"
+                       empty-text="暂无数据"
+                       :expand-on-click-node="false"
+                       :data="list"
+                       :props="permissionTree.defaultProps"
+                       @node-click="handlePermissionNodeClick"
+                       class="input-tree">
+              </el-tree>
+            </el-form-item>
+          </el-col>
+
+          <el-col :span="12">
+            <el-form-item label="备注" prop="remark">
+              <el-input v-model="form.remark" minlength=2></el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+
+        <el-form-item>
+          <el-button type="primary" @click="save">{{ $t('button.submit') }}</el-button>
+          <el-button @click.native="formVisible = false">{{ $t('button.cancel') }}</el-button>
+        </el-form-item>
+      </el-form>
+    </el-dialog>
+
+  </div>
+</template>
+
+<script src="./permission.js"></script>
+<style rel="stylesheet/scss" lang="scss" scoped>
+  @import "src/styles/common.scss";
+</style>
---
+++ manage/src/views/system/permission/permission.js
@@ -0,0 +1,217 @@
+// import { remove, getList, save, savePermissons,getShowList } from '@/api/system/permission'
+import {getList} from '@/api/system/permission'
+import treeTable from '@/components/TreeTable'
+
+export default {
+  name: 'treeTableDemo',
+  components: { treeTable },
+  data() {
+    return {
+      showTree: false,
+      listLoading: true,
+      expandAll: true,
+      formTitle: '',
+      formVisible: false,
+      isAdd: false,
+      formVisible: false,
+      formTitle: '添加权限',
+      permissionList: [],
+      isAdd: true,
+      permissons: [],
+      defaultProps: {
+        id: 'id',
+        label: 'name',
+        children: 'children'
+      },
+      permissonVisible: false,
+      permissionTree: {
+        show: false,
+        defaultProps: {
+          id: 'id',
+          label: 'name',
+          children: 'children'
+        }
+      },
+      form: {
+        id: '',
+        name: '',
+        code: '',
+        pid: 0,
+        pName: '',
+        menuId: '',
+        menuName: '',
+        url: '',
+        displaySeq: 1,
+        remark: ''
+      },
+      rules: {
+        code: [
+          { required: true, message: '请输入权限编码', trigger: 'blur' },
+          { min: 3, max: 50, message: '长度在 3 到 50 个字符', trigger: 'blur' }
+        ],
+        name: [
+          { required: true, message: '请输入角色名称', trigger: 'blur' },
+          { min: 2, max: 50, message: '长度在 2 到 50 个字符', trigger: 'blur' }
+        ]
+      },
+      listQuery: {
+        name: undefined
+      },
+      total: 0,
+      list: null,
+      listLoading: true,
+      selRow: {}
+    }
+  },
+  // filters: {
+  //   statusFilter(status) {
+  //     const statusMap = {
+  //       published: 'success',
+  //       draft: 'gray',
+  //       deleted: 'danger'
+  //     }
+  //     return statusMap[status]
+  //   }
+  // },
+  created() {
+    this.init()
+  },
+  methods: {
+    init() {
+      this.fetchData()
+    },
+    fetchData() {
+      this.listLoading = true
+      getList(this.listQuery).then(response => {
+        console.log("permission fetch data")
+        console.log(response.data)
+        this.list = response.data.records
+        this.listLoading = false
+        this.total = response.data.total
+      })
+    },
+    search() {
+      this.fetchData()
+    },
+    reset() {
+      this.listQuery.name = ''
+      this.fetchData()
+    },
+    handleFilter() {
+      this.getList()
+    },
+    fetchNext() {
+      this.listQuery.page = this.listQuery.page + 1
+      this.fetchData()
+    },
+    fetchPrev() {
+      this.listQuery.page = this.listQuery.page - 1
+      this.fetchData()
+    },
+    fetchPage(page) {
+      this.listQuery.page = page
+      this.fetchData()
+    },
+    changeSize(limit) {
+      this.listQuery.limit = limit
+      this.fetchData()
+    },
+    handleCurrentChange(currentRow, oldCurrentRow) {
+      this.selRow = currentRow
+    },
+    resetForm() {
+      this.form = {
+        id: '',
+        name: '',
+        code: '',
+        pid: 0,
+        pName: '',
+        menuId: '',
+        menuName: '',
+        url: '',
+        displaySeq: 1,
+        remark: ''
+      }
+    },
+    add() {
+      this.resetForm()
+      this.formTitle = '添加权限'
+      this.formVisible = true
+      this.isAdd = true
+    },
+    save() {
+      let self = this
+      this.$refs['form'].validate((valid) => {
+        if (valid) {
+          /**
+           * {
+            id: this.form.id,
+            displaySeq: this.form.displaySeq,
+            parent: this.form.pid,
+            permissionName: this.form.name,
+            permissionCode: this.form.code
+          }
+           */
+          save(self.form).then(response => {
+            this.$message({
+              message: '提交成功',
+              type: 'success'
+            })
+            this.fetchData()
+            this.formVisible = false
+          })
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
+    },
+    checkSel() {
+      if (this.selRow && this.selRow.id) {
+        return true
+      }
+      this.$message({
+        message: '请选中操作项',
+        type: 'warning'
+      })
+      return false
+    },
+    edit() {
+      if (this.checkSel()) {
+        this.isAdd = false
+        this.form = this.selRow
+        this.formTitle = '修改权限'
+        this.formVisible = true
+      }
+    },
+    remove() {
+      if (this.checkSel()) {
+        const id = this.selRow.id
+        this.$confirm('确定删除该记录?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          remove(id).then(response => {
+            this.$message({
+              message: '提交成功',
+              type: 'success'
+            })
+            this.fetchData()
+          }).catch( err=> {
+            this.$notify.error({
+              title: '错误',
+              message:err,
+            })
+          })
+        }).catch(() => {
+        })
+      }
+    },
+    handlepermissionNodeClick(data, node) {
+      this.form.pid = data.id
+      this.form.pName = data.name
+      this.permissionTree.show = false
+    }
+  }
+}
